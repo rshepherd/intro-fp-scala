fed841035d5f5f79b717c9b42e6ac49798412960
Randy Shepherd <randy@giphy.com>
2018-08-17 15:40:18 -0400

    More worksheets

--- exercises/src/main/scala/Ch2.scala
+++ exercises/src/main/scala/Ch2.scala
@@ -5,9 +5,8 @@ object Problems {
   // There are many ways to solve these problems, you can see some
   // idiomatic solutions here http://aperiodic.net/phil/scala/s-99/ for solutions
 
-
   // Reverse a list.
-  def rev[A](list: List[A]): List[A] = ???
+  def rev[A](list: List[A]): List[A] = list.reverse
 
 
   // Find out whether a list is a palindrome.
@@ -30,30 +29,29 @@ object Problems {
 
   // Note that the following functions do not have tests! There is no need
   // since there is only one way to implement it and it can be checked
-  // by the compiler. Static type systems and parametr polymorphism ftw!
+  // by the compiler. Static type systems and parametric polymorphism ftw!
 
   // Hint: use the following implementation of "partial application" for
   // inspiration for the following problems on currying
-  def partial[A,B,C](a: A, f: (A,B) => C): B => C = {
-    (b: B) => f(a, b)
-  }
+  def partial[A,B,C](a: A, f: (A,B) => C): B => C = f(a, _)
+
 
   // Currying!
   // Lets look at another example of dealing with higher order functions, 'currying',
   // which converts a function f of two arguments into a function of one argument that
   // partially applied f. There is only one implementation that compiles, so no need for a test!
-  def curry[A,B,C](a: A, f: (A,B) => C): B => C = ???
+  def curry[A,B,C](a: A, f: (A,B) => C): B => C = (b: B) => f(a, b)
 
 
   // Uncurrying!
   // Implement uncurry, which reverses the transformation of curry. Note that since =>
   // associaltes to the right, A => (B => C) can be written as A => B => C
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C = ???
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a: A, b: B) => f(a)(b)
 
 
   // (Bonus!) Function composition
   // Lets look at at an example of function composition, which feeds the output of one
   // function to the input of another. Again, there is only one correct implementation
   // fully determined by the type signature!
-  def compose[A,B,C](f: A => B, g: B => C): A => C = ???
+  def compose[A,B,C](f: A => B, g: B => C): A => C = (a: A) => g(f(a))
 }
